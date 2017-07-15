81687c06ff2ce7938ebdde7d81c349a24a4e67d7
Agata Zielińska <agata.zielinska@k2.pl>
2017-07-15 13:39:50 +0200

    password

--- app/Entities/User.js
+++ app/Entities/User.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const esdf = require('esdf');
+const bcrypt = require('bcrypt');
 
 class User extends esdf.core.EventSourcedAggregate {
     constructor() {
@@ -8,20 +9,22 @@ class User extends esdf.core.EventSourcedAggregate {
         this._registered = false;
         this._name = null;
         this._email = null;
+        this._password = null;
     }
 
-    register({name, email}) {
-        this._stageEvent(new esdf.core.Event('Registered', { name, email }));
-
-        // this.name = name;
-        // this.email = email;
-        //this._registered = true;
+    async register({name, email, password}) {
+        if (this._registered) {
+            return;
+        }
+        const hash = await bcrypt.hash(password, 10);
+        this._stageEvent(new esdf.core.Event('Registered', { name, email, hash }));
     }
 
     onRegistered(event) {
         this._registered = true;
         this._name = event.eventPayload.name;
         this._email = event.eventPayload.email;
+        this._password = event.eventPayload.hash;
     }
 
     isRegistered() {
--- app/tests/User.test.js
+++ app/tests/User.test.js
@@ -13,12 +13,20 @@ describe('User', function () {
     describe('#register', function () {
         it('should make the user registered', function () {
             const user = new User();
-            user.register({ name: 'Agata', email: 'test@test.pl' });
-            assert.equal(user.isRegistered(), true);
-            assert.equal(user.getName(), 'Agata');
-
-            console.log(user.getStagedEvents());
+            return user.register({ name: 'Agata', email: 'test@test.pl', password: 'test' }).then(function () {
+                assert.equal(user.isRegistered(), true);
+                assert.equal(user.getName(), 'Agata');
+            });
         });
+        it('should be idempotent', function () {
+            const user = new User();
+            return user.register({ name: 'Agata', email: 'test@test.pl', password: 'test' }).then(function () {
+                const beforeCount = user.getStagedEvents().length;
+                user.register({ name: 'Agata', email: 'test@test.pl', password: 'test' });
+                const afterCount = user.getStagedEvents().length;
+                assert.equal(beforeCount, afterCount);
+            });
+        })
     });
 });
 
